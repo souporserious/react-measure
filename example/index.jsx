71d730399f25dbdbc54e27658afa9e823cfb8a70
Travis Arnold <ftntravis@gmail.com>
2015-09-15 19:53:07 -0700

    added all box model values to child function

--- example/index.jsx
+++ example/index.jsx
@@ -1,35 +1,95 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, Children, PropTypes } from 'react';
 import { Spring } from 'react-motion';
 import { Measure } from '../src/index';
 
 import './main.scss';
 
-class App extends Component {
+class Accordion extends Component {  
+  _handleClick(item) {
+    this.props.onClick(item);
+  }
+  
   render() {
+    const { items, active } = this.props;
+    
     return(
-      <div>
-        <Measure>
-        {({height}) =>
-          <Spring
-            defaultValue={0}
-            endValue={height}
+      <ul className="accordion__items">
+        {items.map(item =>
+          <li
+            className="accordion__item" key={item.id}
+            onClick={this._handleClick.bind(this, item)}
           >
-            {(rmHeight) =>
-              <div
-                style={{
-                  height: rmHeight,
-                  padding: 28,
-                  backgroundColor: '#b4da55',
-                  overflow: 'hidden'
-                }}
-              >
-                <h3>height = {Math.ceil(rmHeight)}px</h3>
-                <p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?</p>
-              </div>
-            }
-          </Spring>
+            <h2 className="accordion__item__title">{item.title}</h2>
+            <Measure>
+              {({height}) =>
+                <Spring
+                  endValue={{
+                    val: {
+                      height: active === item.id ? height : 0,
+                      opacity: active === item.id ? 1 : 0,
+                      scale: active === item.id ? 1 : 0.95
+                    }
+                  }}
+                >
+                  {({val: {height, opacity, scale}}) =>
+                    <div
+                      className="accordion__item__content"
+                      style={{height, opacity, transform: `scale(${scale})`}}
+                    >
+                      {item.contents.map((content, i) => <p key={i}>{content}</p>)}
+                    </div>
+                  }
+                </Spring>
+              }
+            </Measure>
+          </li>
+        )}
+      </ul>
+    );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this._handleAccordionClick = this._handleAccordionClick.bind(this);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: 'What is a dog?',
+          contents: ['A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.']
+        }, {
+          id: 1,
+          title: 'What kinds of dogs are there?',
+          contents: ['There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.']
+        }, {
+          id: 2,
+          title: 'How do you acquire a dog?',
+          contents: ['Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.', 'A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.']
         }
-        </Measure>
+      ],
+      active: 1
+    }
+  }
+  
+  _handleAccordionClick({id}) {
+    const active = (id === this.state.active) ? null : id;
+    this.setState({active});
+  }
+  
+  render() {
+    const { items, active } = this.state;
+    const currentSelected = active === null ? 'Nothing selected' : items[active].title;
+
+    return(
+      <div className="app">
+        <strong>Current Selected:</strong> {currentSelected}
+        <Accordion
+          items={items}
+          active={active}
+          onClick={this._handleAccordionClick}
+        />
       </div>
     );
   }
--- src/Measure.jsx
+++ src/Measure.jsx
@@ -36,7 +36,14 @@ function _debounce(fn, delay = 250) {
 class Measure extends Component {
   constructor(props) {
     super(props)
-    this.state = { height: null }
+    this.state = {
+      width: null,
+      height: null,
+      top: null,
+      right: null,
+      bottom: null,
+      left: null
+    }
     this._node = null
     this._nodeCopy = null
     this._nodeParent = null
@@ -47,84 +54,100 @@ class Measure extends Component {
 
   componentWillMount() {
     this._removeClone = _debounce(this._removeClone, this._debounceDelay);
+    this._setMeasure = _throttle(this._setMeasure, 300);
     this._forceMeasure = _throttle(this._forceMeasure, 300);
   }
 
   componentDidMount() {
     this._node = React.findDOMNode(this)
     this._parentNode = this._node.parentNode
-    this.setState({height: this._calcHeight(this._node)});
-    
+    this.setState(this._measure(this._node));
+
+    // need to move into one event listener and delegate updates accordingly
     window.addEventListener('resize', this._forceMeasure);
   }
 
   componentDidUpdate(prevProps, prevState) {
-    let height = this._calcHeight(this._node);
+    const dimensions = this._measure(this._node);
 
-    if(+prevState.height !== +height) {
-      this.setState({height});
+    // we can use JSON stringify to compare objects since they are simple structures to
+    // determine if we need to update our state with new dimensions or not
+    if(JSON.stringify(prevState) !== JSON.stringify(dimensions)) {
+      this._setMeasure(dimensions);
     }
   }
-  
+
   componentWillUnmount() {
     window.removeEventListener('resize', this._forceMeasure);
   }
-  
+
   _forceMeasure() {
     this.forceUpdate();
   }
 
-  _calcHeight(node) {
-    let height
-    
+  _setMeasure(dimensions) {
+    this.setState(dimensions);
+  }
+
+  _measure(node) {
+    let dimensions
+
     if(!this._copyAppended) {
-      const context = document.createElement('div');
-      const copy = node.cloneNode(true);
-      
+      const context = document.createElement('div')
+      const copy = node.cloneNode(true)
+
       // give the node some context to measure off of
-      // height and overflow prevent scrollbars 
-      context.style.height = 0;
-      context.style.position = 'relative';
-      context.style.overflow = 'hidden';
+      // height and overflow prevent scrollbars from copy
+      context.style.height = 0
+      context.style.position = 'relative'
+      context.style.overflow = 'hidden'
       //copy.style.transform = 'translateY(-100%)';
-      
+
       // remove name from all children inputs so they don't conflict with current ones
-      const inputNodes = copy.querySelectorAll('input');
+      const inputNodes = copy.querySelectorAll('input')
       for(let i = inputNodes.length; i--;) {
         inputNodes[i].setAttribute('name', '')
       }
 
       // remove copy and children react id's so it doesn't conflict with current ones
-      const reactNodes = copy.querySelectorAll('[data-reactid]');
-      copy.setAttribute('data-reactid', '');
+      const reactNodes = copy.querySelectorAll('[data-reactid]')
+      copy.setAttribute('data-reactid', '')
       for(let j = reactNodes.length; j--;) {
         reactNodes[j].setAttribute('data-reactid', '')
       }
 
       // set props to hide copy and get true dimensions
-      copy.style.height = 'auto';
-      copy.style.width = '100%';
-      copy.style.position = 'absolute';
-      copy.style.visibility = 'hidden';
-  
+      copy.style.boxSizing = 'border-box'
+      copy.style.height = 'auto'
+      copy.style.width = '100%'
+      copy.style.position = 'absolute'
+      copy.style.visibility = 'hidden'
+
       // append copy to context
-      context.appendChild(copy);
-      
+      context.appendChild(copy)
+
       // append context to DOM so we can measure
       this._parentNode.appendChild(context)
       this._copyAppended = true
-      
+
       // store node so we can calculate on it and remove it later
-      this._nodeCopy = copy;
+      this._nodeCopy = copy
     }
 
-    // grab height of node
-    height = +this._nodeCopy.offsetHeight
-  
+    // grab dimensions of node
+    dimensions = this._nodeCopy.getBoundingClientRect();
+
     // remove the copy after getting it's height
     this._removeClone();
 
-    return height
+    return {
+      width: dimensions.width,
+      height: dimensions.height,
+      top: dimensions.top,
+      right: dimensions.right,
+      bottom: dimensions.bottom,
+      left: dimensions.left
+    }
   }
 
   _removeClone() {
