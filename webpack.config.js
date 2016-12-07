10a6ca4c468b98956568b652a605f29a6f854d55
Travis Arnold <ftntravis@gmail.com>
2016-12-07 10:13:52 -0800

    update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,57 @@
 var path = require('path');
-var nodeModulesDir = path.resolve(__dirname, 'node_modules');
+var webpack = require('webpack');
+var TARGET = process.env.TARGET || null;
 
-module.exports = {
+var config = {
   entry: {
-    index: ['webpack/hot/dev-server', './example/index.jsx']
   },
   output: {
-    path: './example',
+    path: path.join(__dirname, 'example'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.(js|jsx)/, loader: 'babel-loader' },
-      { test: /\.scss$/, loader: 'style!css!postcss!sass?sourceMap' }
+      { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.(css|scss)/, loader: 'style!css!postcss!sass?sourceMap' }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [],
   devServer: {
     contentBase: './example',
+    inline: true
   }
 };
+
+if (TARGET === 'minify') {
+  config.entry.index = path.join(__dirname, 'example/index.jsx');
+
+  config.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }))
+
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    }));
+} else {
+  config.entry.index = [
+    'webpack/hot/dev-server',
+    path.join(__dirname, 'example/index.jsx')
+  ];
+}
+
+module.exports = config;
