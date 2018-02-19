2ac418cb6adf926b9749f8fec5716ef3a12ea356
Travis Arnold <ftntravis@gmail.com>
2018-02-18 21:44:05 -0800

    finish Collapse demo

--- example/components/Collapse.js
+++ example/components/Collapse.js
@@ -1,10 +1,10 @@
 import React, { Component, Children, Fragment, cloneElement } from 'react'
-import { tween, value } from 'popmotion'
+import { tween, value, transform } from 'popmotion'
 import styler from 'stylefire'
 
-import generateFakeData from '../generate-fake-data'
-
 import { Measure } from '../../src/index'
+
+import generateFakeData from '../generate-fake-data'
 
 class Fluid extends Component {
   static defaultProps = {
@@ -12,15 +12,25 @@ class Fluid extends Component {
     height: null,
   }
 
+  state = {
+    isAnimating: false,
+    widthProgress: 0,
+    heightProgress: 0,
+  }
+
   componentWillReceiveProps(nextProps) {
-    if (this.props.width !== nextProps.width && nextProps.width !== null) {
+    if (this.props.width !== nextProps.width) {
       this.animateWidth(
-        nextProps.width === 'auto' ? this.dimensions.width : nextProps.width
+        nextProps.width === 'auto'
+          ? this.dimensions.offsetWidth
+          : nextProps.width
       )
     }
-    if (this.props.height !== nextProps.height && nextProps.height !== null) {
+    if (this.props.height !== nextProps.height) {
       this.animateHeight(
-        nextProps.height === 'auto' ? this.dimensions.height : nextProps.height
+        nextProps.height === 'auto'
+          ? this.dimensions.offsetHeight
+          : nextProps.height
       )
     }
   }
@@ -28,50 +38,82 @@ class Fluid extends Component {
   setRef = node => {
     if (node) {
       this.nodeStyler = styler(node)
-    } else {
-      this.nodeStyler = null
+      this.nodeStyler
+        .set({
+          width: this.props.width,
+          height: this.props.height,
+        })
+        .render()
     }
   }
 
-  animateWidth = amount => {
-    tween({
-      from: this.widthValue.get(),
-      to: amount,
-    }).start(this.widthValue)
+  startAnimating = () => {
+    this.setState({ isAnimating: true })
   }
 
-  animateHeight = amount => {
+  stopAnimating = () => {
+    this.setState({ isAnimating: false })
+  }
+
+  animateDimension = (dimension, toValue) => {
+    const dimensionValue = this[`${dimension}Value`]
+    const fromValue = dimensionValue.get()
+    if (fromValue === toValue) {
+      return
+    }
+    const getProgress = transform.interpolate(
+      [fromValue, toValue],
+      fromValue < toValue ? [0, 1] : [1, 0]
+    )
+    this.startAnimating()
     tween({
-      from: this.heightValue.get(),
-      to: amount,
-    }).start(this.heightValue)
+      from: fromValue,
+      to: toValue,
+    }).start({
+      update: value => {
+        dimensionValue.update(value)
+        this.setState({
+          [`${dimension}Progress`]: getProgress(value),
+        })
+      },
+      complete: this.stopAnimating,
+    })
+  }
+
+  animateWidth = toValue => {
+    this.animateDimension('width', toValue)
+  }
+
+  animateHeight = toValue => {
+    this.animateDimension('height', toValue)
   }
 
   handleMeasure = measurements => {
-    if (!measurements.wrapper) {
-      return
-    }
-    const dimensions = measurements.wrapper
+    const dimensions = measurements.inner
 
     if (this.props.width !== null) {
       if (!this.widthValue) {
         this.widthValue = value(
-          this.props.width === 'auto' ? dimensions.width : this.props.width,
+          this.props.width === 'auto'
+            ? dimensions.offsetWidth
+            : this.props.width,
           this.nodeStyler.set('width')
         )
       } else if (this.props.width === 'auto') {
-        this.animateWidth(dimensions.width)
+        this.widthValue.update(dimensions.offsetWidth)
       }
     }
 
     if (this.props.height !== null) {
       if (!this.heightValue) {
         this.heightValue = value(
-          this.props.height === 'auto' ? dimensions.height : this.props.height,
+          this.props.height === 'auto'
+            ? dimensions.offsetHeight
+            : this.props.height,
           this.nodeStyler.set('height')
         )
       } else if (this.props.height === 'auto') {
-        this.animateHeight(dimensions.height)
+        this.heightValue.update(dimensions.offsetHeight)
       }
     }
 
@@ -79,29 +121,27 @@ class Fluid extends Component {
   }
 
   render() {
-    const {
-      width,
-      height,
-      innerProps,
-      children,
-      style,
-      ...restProps
-    } = this.props
+    const { width, height, innerProps, children, ...restProps } = this.props
+    const { isAnimating, widthProgress, heightProgress } = this.state
     return (
-      <div ref={this.setRef} style={{ width, height, ...style }} {...restProps}>
-        <Measure onMeasure={this.handleMeasure}>
-          {({ bind }) => (
-            <div {...bind('wrapper')} {...innerProps}>
-              {children}
-            </div>
-          )}
-        </Measure>
-      </div>
+      <Measure onMeasure={this.handleMeasure}>
+        {({ bind }) =>
+          children({
+            bindWrapper: { ref: this.setRef },
+            bindInner: bind('inner'),
+            isAnimating,
+            widthProgress,
+            heightProgress,
+          })
+        }
+      </Measure>
     )
   }
 }
 
-class CollapseDemo extends Component {
+const getScale = transform.interpolate([0, 1], [0.95, 1])
+
+class Collapse extends Component {
   state = {
     isOpen: false,
   }
@@ -111,21 +151,35 @@ class CollapseDemo extends Component {
     return (
       <div>
         <button onClick={() => this.setState({ isOpen: !isOpen })}>
-          Toggle
+          Toggle collapsed content
         </button>
-        <Fluid
-          height={isOpen ? 'auto' : 0}
-          style={{ overflow: 'hidden', background: 'orange' }}
-          innerProps={{
-            style: { padding: 12 },
-          }}
-        >
-          This is some sample content that will show how a collapse can break up
-          multiple sections of content and still animate.
+        <Fluid height={isOpen ? 'auto' : 0}>
+          {({ bindWrapper, bindInner, isAnimating, heightProgress }) => (
+            <div
+              {...bindWrapper}
+              style={{
+                backgroundColor: 'rgba(0,0,0,0.05)',
+                overflow: (!isOpen || isAnimating) && 'hidden',
+              }}
+            >
+              <div
+                {...bindInner}
+                style={{
+                  padding: 12,
+                  opacity: heightProgress,
+                  transform: `scale(${getScale(heightProgress)})`,
+                }}
+              >
+                This is some sample content that will show how a collapse can
+                break up multiple sections of content and still animate between
+                component resizes.
+              </div>
+            </div>
+          )}
         </Fluid>
       </div>
     )
   }
 }
 
-export default CollapseDemo
+export default Collapse
