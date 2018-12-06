14e725e496fb0ac34f0ada7b011d134d4ee0872d
Travis Arnold <ftntravis@gmail.com>
2018-12-05 22:57:05 -0800

    move site to gatsby, cleanup build process

---
+++ site/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pathPrefix: '/react-measure',
+  siteMetadata: {
+    title: '📏 React Measure',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+      },
+    },
+  ],
+}
